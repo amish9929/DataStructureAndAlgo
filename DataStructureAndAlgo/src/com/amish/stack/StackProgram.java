d6a5bb59c7fcfd29f2470e8cd7b4c3aebc150ba8
amish9929 <amish1406@gmail.com>
2017-01-22 23:08:47 -0800

    First simple output change

--- DataStructureAndAlgo/src/com/amish/stack/StackProgram.java
+++ DataStructureAndAlgo/src/com/amish/stack/StackProgram.java
@@ -5,7 +5,9 @@ public class StackProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println();
+		System.out.println("TEST");
+		
+		System.out.println("TEST 1");
 	}
 
 }
